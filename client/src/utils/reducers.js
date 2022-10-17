502befb2b3fc514ba74edefd881a2aa28d0c28d6
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-10-17 19:24:12 -0400

    cart additions

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -3,8 +3,14 @@ import { useReducer } from 'react';
 import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY
-} from "./actions";
+  UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART
+} from './actions';
 
 export const reducer = (state, action) => {
   switch (action.type) {
@@ -13,6 +19,31 @@ export const reducer = (state, action) => {
       return {
         ...state,
         products: [...action.products],
+      };
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product]
+      };
+
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: []
+      };
+
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen
       };
 
     // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
@@ -28,11 +59,35 @@ export const reducer = (state, action) => {
         currentCategory: action.currentCategory
       };
 
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter(product => {
+        return product._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState
+      };
+
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map(product => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        })
+      };
+
     // if it's none of these actions, do not update state at all and keep things the same!
     default:
       return state;
   }
 };
+
 
 export function useProductReducer(initialState) {
   return useReducer(reducer, initialState);
