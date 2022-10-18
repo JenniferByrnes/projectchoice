c2b88f17b099679b21de6a5690319224869ef229
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-10-18 12:32:13 -0400

    indexdb implemented

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART } from '../../utils/actions';
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 import './style.css';
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
+
+  // Now let's set up the useEffect() Hook in the Cart component's function to check if there's anything in the state's cart property on load. If not, we'll retrieve data from the IndexedDB cart object store.
+  useEffect(() => {
+    async function getCart() {
+      const cart = await idbPromise('cart', 'get');
+      dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+    };
+  
+    if (!state.cart.length) {
+      getCart();
+    }
+  }, [state.cart.length, dispatch]);
 
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
