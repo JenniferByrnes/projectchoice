cf6c7f5d71e1053d73ec79e074e7e7a0ef654471
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-10-25 15:19:35 -0400

    feature/stripe

---
+++ client/build/precache-manifest.d363ceb71c25f656d07ac70084712791.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69633a38e86d99d6e2fa6ae6b714dbef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ddf66cb0095590c36f5",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "8a796f660bda6ecaf19c",
+    "url": "/static/js/2.b091bb1c.chunk.js"
+  },
+  {
+    "revision": "539e9828e086173d6c8e16e3602b9bcd",
+    "url": "/static/js/2.b091bb1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ddf66cb0095590c36f5",
+    "url": "/static/js/main.a5bbdb68.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.a5bbdb68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
