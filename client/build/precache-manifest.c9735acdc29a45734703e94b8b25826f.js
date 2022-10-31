9f83f230bb022109af37823a5da894a9e1e22e69
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-10-30 23:28:05 -0400

    messing around

---
+++ client/build/precache-manifest.c9735acdc29a45734703e94b8b25826f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84149756c3a8aef1b51ef1974b7e815a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53b544d36e5ec0c1f01d",
+    "url": "/static/css/main.2733a41b.chunk.css"
+  },
+  {
+    "revision": "00de31621000af26efc5",
+    "url": "/static/js/2.5ef5e43a.chunk.js"
+  },
+  {
+    "revision": "539e9828e086173d6c8e16e3602b9bcd",
+    "url": "/static/js/2.5ef5e43a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b544d36e5ec0c1f01d",
+    "url": "/static/js/main.6e978931.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.6e978931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b28e8916ded4a56104f1",
+    "url": "/static/js/runtime-main.ee2bb25a.js"
+  },
+  {
+    "revision": "b7a8a598b2bea1245054208ccd8bb543",
+    "url": "/static/media/fullLogo.b7a8a598.png"
+  },
+  {
+    "revision": "a452b98ab198c2ae47b46dc86504f941",
+    "url": "/static/media/poster.a452b98a.jpeg"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
